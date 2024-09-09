42d01746b3c6e1f22bc13ede8ebcfeee551eb54b
Anthony Sena <asena5@its.jnj.com>
2024-09-09 11:03:41 -0400

    Add cohorts

--- extras/DownloadExampleCohorts.R
+++ extras/DownloadExampleCohorts.R
@@ -1,9 +1,11 @@
+library(dplyr)
+baseUrl <- keyring::key_get("webApiUrl", keyring = "ohda")
 ROhdsiWebApi::authorizeWebApi(
-  baseUrl = keyring::key_get("webApiUrl", keyring = "ohda"),
+  baseUrl = baseUrl,
   authMethod = "windows"
 )
 cohortDefinitionSet <- ROhdsiWebApi::exportCohortDefinitionSet(
-  baseUrl = keyring::key_get("webApiUrl", keyring = "ohda"),
+  baseUrl = baseUrl,
   cohortIds = c(
     3909, # New users of Celecoxib
     5903, # New users of Diclofenac
@@ -22,3 +24,30 @@ cohortDefinitionSet[cohortDefinitionSet$cohortId == 3909,]$cohortId <- 1
 cohortDefinitionSet[cohortDefinitionSet$cohortId == 5903,]$cohortId <- 2
 cohortDefinitionSet[cohortDefinitionSet$cohortId == 5904,]$cohortId <- 3
 
+# Save the cohort definition set
+CohortGenerator::saveCohortDefinitionSet(
+  cohortDefinitionSet = cohortDefinitionSet
+)
+
+
+# Download and save the negative control outcomes
+negativeControlOutcomeCohortSet <- ROhdsiWebApi::getConceptSetDefinition(
+  conceptSetId = 1720,
+  baseUrl = baseUrl
+) %>%
+  ROhdsiWebApi::resolveConceptSet(
+    baseUrl = baseUrl
+  ) %>%
+  ROhdsiWebApi::getConcepts(
+    baseUrl = baseUrl
+  ) %>%
+  rename(outcomeConceptId = "conceptId",
+         cohortName = "conceptName") %>%
+  mutate(cohortId = row_number() + 1000) %>%
+  select(cohortId, cohortName, outcomeConceptId)
+
+CohortGenerator::writeCsv(
+  x = negativeControlOutcomeCohortSet,
+  file = "inst/negativeControlOutcomes.csv",
+  warnOnFileNameCaseMismatch = F
+)
