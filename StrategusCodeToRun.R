577f63ab3ace6c3d0f78aade80b96c487f8b3a49
Anthony Sena <asena5@its.jnj.com>
2024-09-26 08:42:31 -0400

    Bump Strategus, CG, SqlRender

--- StrategusCodeToRun.R
+++ StrategusCodeToRun.R
@@ -17,12 +17,12 @@ Sys.setenv("_JAVA_OPTIONS"="-Xmx4g") # Sets the Java maximum heap space to 4GB
 Sys.setenv("VROOM_THREADS"=1) # Sets the number of threads to 1 to avoid deadlocks on file system
 
 ##=========== START OF INPUTS ==========
-cdmDatabaseSchema <- "merative_mdcd.cdm_merative_mdcd_v3038"
+cdmDatabaseSchema <- "iqvia_france.cdm_iqvia_france_v2914"
 workDatabaseSchema <- "scratch.scratch_asena5"
 outputLocation <- file.path(getwd(), "results")
-databaseName <- "merative_mdcd_v3038" # Only used as a folder name for results from the study
+databaseName <- "cdm_iqvia_france_v2914" # Only used as a folder name for results from the study
 minCellCount <- 5
-cohortTableName <- "strategus_repo_test_cdm_merative_mdcd_v3038"
+cohortTableName <- "strategus_repo_test_cdm_iqvia_france_v2914"
 
 # Create the connection details for your CDM
 # More details on how to do this are found here:
@@ -59,9 +59,6 @@ ParallelLogger::saveSettingsToJson(
   object = executionSettings,
   fileName = file.path(outputLocation, databaseName, "executionSettings.json")
 )
-
-# CM Only
-analysisSpecifications$moduleSpecifications <- analysisSpecifications$moduleSpecifications[5]
 
 Strategus::execute(
   analysisSpecifications = analysisSpecifications,
