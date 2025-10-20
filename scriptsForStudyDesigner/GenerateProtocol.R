c79eb0d3901184c5b98daf057f1fb2139db4d94e
Anthony Sena <asena5@its.jnj.com>
2025-10-19 21:00:31 -0400

    Add protocol generator script for designer

--- scriptsForStudyDesigner/GenerateProtocol.R
+++ scriptsForStudyDesigner/GenerateProtocol.R
@@ -3,7 +3,7 @@ config <- config::get()
 # open this inside your strategus project or set the directory
 # using setwd('location to strategus directory')
 ProtocolGenerator::generateProtocol(
-  jsonLocation = file.path("inst", "sampleStudy", "sampleStudyAnalysisSpecification.json"),
+  jsonLocation = file.path(getwd(), "inst", "sampleStudy", "sampleStudyAnalysisSpecification.json"),
   webAPI = config$webApiUrl, 
   outputLocation = '../extras', 
   outputName = 'protocol.html', 
